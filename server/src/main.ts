d97db1498b927185747e4500e42d47893634330f
Na3ar-17 <gavruk2017@gmail.com>
2024-08-26 14:03:00 +0200

    initial commit

---
+++ server/src/main.ts
@@ -0,0 +1,38 @@
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import * as dotenv from 'dotenv'
+import express, { Express, Request, Response } from 'express'
+
+dotenv.config()
+
+const app: Express = express()
+
+async function main() {
+	app.use(bodyParser.json())
+
+	app.use(
+		cors({
+			origin: ['http://localhost:5173'],
+		})
+	)
+
+	app.get('/', (req: Request, res: Response) => {
+		res.send('init')
+	})
+
+	app.all('*', (req: Request, res: Response) => {
+		res.status(404).json({ error: `Route ${req.originalUrl} not found` })
+	})
+
+	const PORT = process.env.PORT || 4200
+
+	try {
+		app.listen(PORT, () => {
+			console.log(`Server is running at http://localhost:${PORT}`)
+		})
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+main()
