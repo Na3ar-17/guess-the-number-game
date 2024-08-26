33b2cbf3e263c56dd3f6a72810b90e04804a61d1
Na3ar-17 <gavruk2017@gmail.com>
2024-08-26 14:40:16 +0200

    created method guess

--- server/src/game/game.controller.ts
+++ server/src/game/game.controller.ts
@@ -1,13 +1,30 @@
 import { Request, Response, Router } from 'express'
+import { body, validationResult } from 'express-validator'
 import { gameService } from './game.service'
-
 const router = Router()
 
 router.post('/start-game', (req: Request, res: Response) => {
 	const number = gameService.startGame()
-	res.json({
+	res.status(200).json({
 		number,
 	})
 })
+router.post(
+	'/guess',
+	body('number').isFloat({ min: 1, max: 100 }),
+	(req: Request, res: Response) => {
+		const errors = validationResult(req)
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() })
+		}
+		const dto: { number: number } = req.body
+		const { message } = gameService.guess(dto.number)
+
+		res.status(200).json({
+			message,
+		})
+	}
+)
 
 export { router as gameRouter }
--- server/src/game/game.service.ts
+++ server/src/game/game.service.ts
@@ -1,8 +1,26 @@
 class GameService {
 	generatedNumber: number = 0
-	startGame() {
+	startGame(): number {
 		this.generatedNumber = Math.floor(Math.random() * 100)
 		return this.generatedNumber
+	}
+
+	guess(number: number): { message: string } {
+		if (number === this.generatedNumber) {
+			return {
+				message: 'Число вгадано',
+			}
+		}
+
+		if (number > this.generatedNumber) {
+			return {
+				message: 'Загадане число менше',
+			}
+		}
+
+		return {
+			message: 'Загадане число більше',
+		}
 	}
 }
 
