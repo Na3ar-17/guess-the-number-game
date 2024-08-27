2c8fa5c5d7bdf4442b62fd3e0a432f91c44b958c
Na3ar-17 <gavruk2017@gmail.com>
2024-08-27 11:39:28 +0200

    implemented telegram bot

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,38 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import * as dotenv from 'dotenv'
 import express, { Express, Request, Response } from 'express'
+import TelegramBot from 'node-telegram-bot-api'
 import { gameRouter } from './game/game.controller'
 
 dotenv.config()
 
 const app: Express = express()
+
+const token = process.env.TELEGRAM_BOT_TOKEN
+const bot = new TelegramBot(token || '', { polling: true })
+
+bot.onText(/\/start/, msg => {
+	const chatId = msg.chat.id
+
+	const startKeyboard = {
+		reply_markup: {
+			inline_keyboard: [
+				[
+					{
+						text: 'Почати гру',
+						url: 'https://www.npmjs.com/package/node-telegram-bot-api',
+					},
+				],
+			],
+		},
+	}
+
+	bot.sendMessage(
+		chatId,
+		'Вітаю у грі! Натисніть кнопку щоб почати.',
+		startKeyboard
+	)
+})
 
 async function main() {
 	app.use(bodyParser.json())
